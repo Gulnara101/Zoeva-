fc7519beea42788c6036a0d333edcab9a28630bf
Gulnara101 <mrs.gul99@gmail.com>
2025-02-05 20:38:55 +0400

    header hover

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,21 @@ import { Link } from "react-router-dom";
 import { IoSearchOutline } from "react-icons/io5";
 import { CiUser, CiShoppingBasket } from "react-icons/ci";
 import Logo from "../../Images/svg/logo.svg";
-import navBarData from "../../Mocks/headerData";
+import BrushSets from "../Header/BrushSets";
+import Brushes from "../Header/Brushes";
+import Face from "../Header/Face";
+import Eyes from "../Header/Eyes";
+import Lips from "../Header/Lips";
+import Accessories from "../Header/Accessories";
 
 const Header = () => {
+  const [activeSets, setActiveSets] = useState(false);
+  const [activeBrush, setActiveBrush] = useState(false);
+  const [activeFace, setActiveFace] = useState(false);
+  const [activeEyes, setActiveEyes] = useState(false);
+  const [activeLips, setActiveLips] = useState(false);
+  const [activeAccessories, setActiveAccessories] = useState(false);
+
   return (
     <header>
       <div className="row">
@@ -16,7 +28,7 @@ const Header = () => {
         </div>
         <div className="headerActions">
           <div className="logo">
-            <img src={Logo} alt="" />
+            <img src={Logo} alt="Logo" />
           </div>
           <div className="userControls">
             <IoSearchOutline className="icon" />
@@ -28,7 +40,6 @@ const Header = () => {
             <div className="quantities">
               <span>1</span>
             </div>
-            {}
           </div>
         </div>
         <nav className="navBar">
@@ -38,11 +49,54 @@ const Header = () => {
                 NEW
               </Link>
             </li>
-            {navBarData.map((item) => (
-              <li key={item.id} className="navItem">
-                <Link to="#">{item.title}</Link>
-              </li>
-            ))}
+            <li
+              className="navItem"
+              onMouseEnter={() => setActiveSets(true)}
+              onMouseLeave={() => setActiveSets(false)}
+            >
+              <Link to="#">BRUSH SETS</Link>
+              {activeSets && <BrushSets />}
+            </li>
+            <li
+              className="navItem"
+              onMouseEnter={() => setActiveBrush(true)}
+              onMouseLeave={() => setActiveBrush(false)}
+            >
+              <Link to="#">BRUSHES</Link>
+              {activeBrush && <Brushes />}
+            </li>
+            <li
+              className="navItem"
+              onMouseEnter={() => setActiveFace(true)}
+              onMouseLeave={() => setActiveFace(false)}
+            >
+              <Link to="#">FACE</Link>
+              {activeFace && <Face />}
+            </li>
+            <li
+              className="navItem"
+              onMouseEnter={() => setActiveEyes(true)}
+              onMouseLeave={() => setActiveEyes(false)}
+            >
+              <Link to="#">EYES</Link>
+              {activeEyes && <Eyes />}
+            </li>
+            <li
+              className="navItem"
+              onMouseEnter={() => setActiveLips(true)}
+              onMouseLeave={() => setActiveLips(false)}
+            >
+              <Link to="#">LIPS</Link>
+              {activeLips && <Lips />}
+            </li>
+            <li
+              className="navItem"
+              onMouseEnter={() => setActiveAccessories(true)}
+              onMouseLeave={() => setActiveAccessories(false)}
+            >
+              <Link to="#">ACCESSORIES</Link>
+              {activeAccessories && <Accessories />}
+            </li>
           </ul>
         </nav>
       </div>
